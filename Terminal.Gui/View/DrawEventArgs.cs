522e3bd7d18721ad647ccd52ce43799d54e4af14
Brandon Thetford <github@snapsinazfs.com>
2024-01-27 15:01:17 -0700

    Pull these out to their own files per class

---
+++ Terminal.Gui/View/DrawEventArgs.cs
@@ -0,0 +1,28 @@
+﻿#nullable enable
+
+namespace Terminal.Gui;
+
+/// <summary>
+/// Event args for draw events
+/// </summary>
+public class DrawEventArgs : EventArgs {
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="DrawEventArgs"/> class.
+    /// </summary>
+    /// <param name="rect">Gets the view-relative rectangle describing the currently visible viewport into the <see cref="View"/>.</param>
+    public DrawEventArgs (Rect rect)
+    {
+        Rect = rect;
+    }
+
+    /// <summary>
+    /// Gets the view-relative rectangle describing the currently visible viewport into the <see cref="View"/>.
+    /// </summary>
+    public Rect Rect { get; }
+
+    /// <summary>
+    /// If set to true, the draw operation will be canceled, if applicable.
+    /// </summary>
+    public bool Cancel { get; set; }
+}
---
+++ Terminal.Gui/View/LayoutEventArgs.cs
@@ -0,0 +1,12 @@
+﻿#nullable enable
+namespace Terminal.Gui;
+
+/// <summary>
+/// Event arguments for the <see cref="View.LayoutComplete"/> event.
+/// </summary>
+public class LayoutEventArgs : EventArgs {
+    /// <summary>
+    /// The view-relative bounds of the <see cref="View"/> before it was laid out.
+    /// </summary>
+    public Rect OldBounds { get; set; }
+}
