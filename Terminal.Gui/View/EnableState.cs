a6c87a1fe85284701d4ecac31b25bd0415818f3c
Brandon Thetford <github@snapsinazfs.com>
2024-02-05 02:35:55 -0700

    Add missing enum

---
+++ Terminal.Gui/View/EnableState.cs
@@ -0,0 +1,20 @@
+#nullable enable
+
+namespace Terminal.Gui;
+
+/// <summary>
+/// An enumeration describing enable states.
+/// </summary>
+/// <remarks>See containing type for specific use and behavior of each value.</remarks>
+public enum EnableState
+{
+    /// <summary>Default. Typically, means inherit from the object's parent.</summary>
+    /// <remarks>See containing type for specific use and behavior.</remarks>
+    Default,
+    /// <summary>Enabled.</summary>
+    /// <remarks>See containing type for specific use and behavior.</remarks>
+    Enabled,
+    /// <summary>Disabled.</summary>
+    /// <remarks>See containing type for specific use and behavior.</remarks>
+    Disabled
+}
