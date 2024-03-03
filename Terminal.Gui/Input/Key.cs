fe4e6ddc2872c6ec5e7bc451b28890d9ad4e5313
Brandon Thetford <github@snapsinazfs.com>
2024-03-02 19:39:45 -0700

    Pull operators out into a nested file

--- Terminal.Gui/Input/Key.cs
+++ Terminal.Gui/Input/Key.cs
@@ -70,7 +70,7 @@ namespace Terminal.Gui;
 ///         </list>
 ///     </para>
 /// </remarks>
-public sealed record Key : IEqualityOperators<Key,Key,bool>
+public sealed partial record Key : IEqualityOperators<Key,Key,bool>
 {
     /// <summary>Constructs a new <see cref="Key"/></summary>
     public Key () : this (KeyCode.Null) { }
@@ -127,23 +127,6 @@ public sealed record Key : IEqualityOperators<Key,Key,bool>
 
         KeyCode = key.KeyCode;
     }
-
-    /// <summary>
-    ///     The key value as a Rune. This is the actual value of the key pressed, and is independent of the modifiers.
-    ///     Useful for determining if a key represents is a printable character.
-    /// </summary>
-    /// <remarks>
-    ///     <para>Keys with Ctrl or Alt modifiers will return <see langword="default"/>.</para>
-    ///     <para>
-    ///         If the key is a letter key (A-Z), the Rune will be the upper or lower case letter depending on whether
-    ///         <see cref="KeyCode.ShiftMask"/> is set.
-    ///     </para>
-    ///     <para>
-    ///         If the key is outside of the <see cref="KeyCode.CharMask"/> range, the returned Rune will be
-    ///         <see langword="default"/>.
-    ///     </para>
-    /// </remarks>
-    public Rune AsRune => ToRune (KeyCode);
 
     private bool _handled = false;
 
@@ -292,130 +275,7 @@ public sealed record Key : IEqualityOperators<Key,Key,bool>
         return (keyCode & KeyCode.CharMask) is >= KeyCode.A and <= KeyCode.Z;
     }
 
-    /// <summary>
-    ///     Converts a <see cref="KeyCode"/> to a <see cref="Rune"/>. Useful for determining if a key represents is a
-    ///     printable character.
-    /// </summary>
-    /// <remarks>
-    ///     <para>Keys with Ctrl or Alt modifiers will return <see langword="default"/>.</para>
-    ///     <para>
-    ///         If the key is a letter key (A-Z), the Rune will be the upper or lower case letter depending on whether
-    ///         <see cref="KeyCode.ShiftMask"/> is set.
-    ///     </para>
-    ///     <para>
-    ///         If the key is outside of the <see cref="KeyCode.CharMask"/> range, the returned Rune will be
-    ///         <see langword="default"/>.
-    ///     </para>
-    /// </remarks>
-    /// <param name="key"></param>
-    /// <returns>The key converted to a Rune. <see langword="default"/> if conversion is not possible.</returns>
-    public static Rune ToRune (KeyCode key)
-    {
-        if (key is KeyCode.Null or KeyCode.SpecialMask
-            || key.HasFlag (KeyCode.CtrlMask)
-            || key.HasFlag (KeyCode.AltMask))
-        {
-            return default (Rune);
-        }
-
-        // Extract the base key code
-        KeyCode baseKey = key;
-
-        if (baseKey.HasFlag (KeyCode.ShiftMask))
-        {
-            baseKey &= ~KeyCode.ShiftMask;
-        }
-
-        switch (baseKey)
-        {
-            case >= KeyCode.A and <= KeyCode.Z when !key.HasFlag (KeyCode.ShiftMask):
-                return new Rune ((uint)(baseKey + 32));
-            case >= KeyCode.A and <= KeyCode.Z when key.HasFlag (KeyCode.ShiftMask):
-                return new Rune ((uint)baseKey);
-            case > KeyCode.Null and < KeyCode.A:
-                return new Rune ((uint)baseKey);
-        }
-
-        if (Enum.IsDefined (typeof (KeyCode), baseKey))
-        {
-            return default (Rune);
-        }
-
-        return new Rune ((uint)baseKey);
-    }
-
-    #region Operators
-
-    /// <summary>
-    ///     Explicitly cast a <see cref="Key"/> to a <see cref="Rune"/>. The conversion is lossy because properties such
-    ///     as <see cref="Handled"/> are not encoded in <see cref="KeyCode"/>.
-    /// </summary>
-    /// <remarks>Uses <see cref="AsRune"/>.</remarks>
-    /// <param name="kea"></param>
-    public static explicit operator Rune (Key kea) { return kea.AsRune; }
-
     // BUGBUG: (Tig) I do not think this cast operator is really needed. 
-    /// <summary>
-    ///     Explicitly cast <see cref="Key"/> to a <see langword="uint"/>. The conversion is lossy because properties such
-    ///     as <see cref="Handled"/> are not encoded in <see cref="KeyCode"/>.
-    /// </summary>
-    /// <param name="kea"></param>
-    public static explicit operator uint (Key kea) { return (uint)kea.KeyCode; }
-
-    /// <summary>
-    ///     Explicitly cast <see cref="Key"/> to a <see cref="KeyCode"/>. The conversion is lossy because properties such
-    ///     as <see cref="Handled"/> are not encoded in <see cref="KeyCode"/>.
-    /// </summary>
-    /// <param name="key"></param>
-    public static explicit operator KeyCode (Key key) { return key.KeyCode; }
-
-    /// <summary>Cast <see cref="KeyCode"/> to a <see cref="Key"/>.</summary>
-    /// <param name="keyCode"></param>
-    public static implicit operator Key (KeyCode keyCode) { return new Key (keyCode); }
-
-    /// <summary>Cast <see langword="char"/> to a <see cref="Key"/>.</summary>
-    /// <remarks>See <see cref="Key(char)"/> for more information.</remarks>
-    /// <param name="ch"></param>
-    public static implicit operator Key (char ch) { return new Key (ch); }
-
-    /// <summary>Cast <see langword="string"/> to a <see cref="Key"/>.</summary>
-    /// <remarks>See <see cref="Key(string)"/> for more information.</remarks>
-    /// <param name="str"></param>
-    public static implicit operator Key (string str) { return new Key (str); }
-
-    /// <summary>Cast a <see cref="Key"/> to a <see langword="string"/>.</summary>
-    /// <remarks>See <see cref="Key(string)"/> for more information.</remarks>
-    /// <param name="key"></param>
-    public static implicit operator string (Key key) { return key.ToString (); }
-
-    /// <inheritdoc/>
-    public override int GetHashCode () { return (int)KeyCode; }
-
-    /// <summary>Compares two <see cref="Key"/>s for less-than.</summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static bool operator < (Key a, Key b) { return a?.KeyCode < b?.KeyCode; }
-
-    /// <summary>Compares two <see cref="Key"/>s for greater-than.</summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static bool operator > (Key a, Key b) { return a?.KeyCode > b?.KeyCode; }
-
-    /// <summary>Compares two <see cref="Key"/>s for greater-than-or-equal-to.</summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static bool operator <= (Key a, Key b) { return a?.KeyCode <= b?.KeyCode; }
-
-    /// <summary>Compares two <see cref="Key"/>s for greater-than-or-equal-to.</summary>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    public static bool operator >= (Key a, Key b) { return a?.KeyCode >= b?.KeyCode; }
-
-    #endregion Operators
 
     #region String conversion
 
